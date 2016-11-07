a47b4a668a929946e623f7dfe35c7cc3f4d291b8
Daniel Derhami <daniel.derhami@gmail.com>
2016-11-07 01:05:48 +0000

    CRUDService

---
+++ service/CRUDService.java
@@ -0,0 +1,15 @@
+package service;
+
+import model.Boat;
+import model.Member;
+import model.WorkShopException;
+import view.StartApp;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * this class prepare data for saving in file , use FileService class for doing
+ * this
+ */
