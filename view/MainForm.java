5eba26be77c81136079d1199e7b5c8d7b820a026
Daniel Derhami <daniel.derhami@gmail.com>
2016-11-10 00:03:26 +0000

    MainForm

--- view/MainForm.java
+++ view/MainForm.java
@@ -2,4 +2,14 @@
 package view;
 import controller.FrameActionListener;
 
-public class MainForm extends javax.swing.JFrame {}
+public class MainForm extends javax.swing.JFrame {
+    FrameActionListener frameActionListener = null;
+
+    public MainForm() {
+        frameActionListener = new FrameActionListener(this);
+        initComponents();
+
+    }
+
+
+}
